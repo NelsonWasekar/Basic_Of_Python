0c97438c3d138a6e589493e9b2865f6c188008b8
Nelson <62866867+NelsonJyostna@users.noreply.github.com>
2024-04-05 13:14:04 +0530

    Not Equal To Operator

---
+++ Not_Equal_To.py
@@ -0,0 +1,16 @@
+
+#See_the_differences
+
+print(10!=10)
+
+print(9!=8)
+
+a=2
+while(a!=5):                              #Not equal to using while
+    print("Hello")
+    a=a+1
+
+a=2
+while(a!=5):
+    print("Bye Bye")
+    break;                          #break ek hi baar execute ho raha hey
