0dd3d5bb2eb7293efb07b1b94e9f4f64ac68d7ba
Nelson <62866867+NelsonJyostna@users.noreply.github.com>
2024-04-05 13:15:15 +0530

    Learn Range Function

---
+++ Range_Function.py
@@ -0,0 +1,24 @@
+
+#Range_Fucntion
+
+
+print(list(range(1,10)))
+print(list(range(1,20,2)))
+print(list(range(20,1, -1)))
+
+
+
+
+print(list(range(1,21,1)))
+
+
+print(list(range(10)))
+
+print(list(range(0,10,2)))
+
+
+print(list(range(-1,-10,-1)))
+
+print(list(range(5)))
+
+
